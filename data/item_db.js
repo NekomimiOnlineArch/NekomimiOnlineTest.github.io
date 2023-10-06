8085cba7d3e012e03036cdb57af83342bd226535
Mystery Coder X <kamaitachi9000@gmail.com>
2023-10-06 18:06:15 +0800

    ni

---
+++ data/item_db.js
@@ -0,0 +1,40 @@
+var itemDB = {
+	"1": {
+			"Id": 1201,
+			"AegisName": "Knife",
+			"Name": "Knife",
+			"Type": "IT_WEAPON",
+			"Buy": 50,
+			"Weight": 400,
+			"Atk": 17,
+			"Range": 1,
+			"Slots": 3,
+			"Job": {
+				"Novice": true,
+				"Swordsman": true,
+				"Magician": true,
+				"Archer": true,
+				"Merchant": true,
+				"Thief": true,
+				"Knight": true,
+				"Wizard": true,
+				"Blacksmith": true,
+				"Hunter": true,
+				"Assassin": true,
+				"Crusader": true,
+				"Sage": true,
+				"Rogue": true,
+				"Alchemist": true,
+				"Bard": true,
+				"Soul_Linker": true,
+				"Ninja": true,
+				"Kagerou": true,
+				"Rebellion": true
+			},
+			"Loc": "EQP_WEAPON",
+			"WeaponLv": 1,
+			"EquipLv": 1,
+			"Subtype": "W_DAGGER"
+		}
+
+}
